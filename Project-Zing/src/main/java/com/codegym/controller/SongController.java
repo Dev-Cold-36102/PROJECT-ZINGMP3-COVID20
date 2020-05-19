84bcc686abf6c01513d80b0a07e44549f6ceca45
lam300695 <lam300695@gmail.com>
2020-05-19 11:04:58 +0700

    Sua + Xoa

---
+++ Project-Zing/src/main/java/com/codegym/controller/SongController.java
@@ -0,0 +1,68 @@
+package com.codegym.controller;
+
+import com.codegym.model.Song;
+import com.codegym.service.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+public class SongController {
+
+    @Autowired
+    private SongService songService;
+
+    @GetMapping("/api/songs")
+    @ResponseBody()
+    public Iterable<Song> getListCustomers(){
+        Iterable<Song> songs = songService.findAll();
+        return songs;
+    }
+
+//    @GetMapping("/api/view/{id}")
+//    public ResponseEntity<Void> viewSong(@PathVariable Long id){
+//        Song song = songService.findById(id);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+
+    @RequestMapping(value = "/songs/update/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Song> updateCustomer(@PathVariable("id") int id, @RequestBody Song song) {
+        System.out.println("Updating Song " + id);
+
+        Song currentSong = songService.findById(id);
+
+        if (currentSong == null) {
+            System.out.println("Song with id " + id + " not found");
+            return new ResponseEntity<Song>(HttpStatus.NOT_FOUND);
+        }
+
+        currentSong.setNameSong(song.getNameSong());
+        currentSong.setInfoSong(song.getInfoSong());
+        currentSong.setImageSong(song.getImageSong());
+        currentSong.setDateSong(song.getDateSong());
+        currentSong.setLikeSong(song.getLikeSong());
+        currentSong.setListenSong(song.getListenSong());
+        currentSong.setDownloadSong(song.getDownloadSong());
+        currentSong.setCommendSong(song.getCommendSong());
+
+        songService.save(currentSong);
+        return new ResponseEntity<Song>(currentSong, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/songs/delete/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Song> deleteCustomer(@PathVariable("id") int id) {
+        System.out.println("Fetching & Deleting Song with id " + id);
+
+        Song song = songService.findById(id);
+        if (song == null) {
+            System.out.println("Unable to delete. Song with id " + id + " not found");
+            return new ResponseEntity<Song>(HttpStatus.NOT_FOUND);
+        }
+
+        songService.remove(id);
+        return new ResponseEntity<Song>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ Project-Zing/src/main/java/com/codegym/model/Song.java
@@ -0,0 +1,108 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "songs")
+public class Song  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int idSong;
+    private String nameSong;
+    private String infoSong;
+    private String imageSong;
+    public String dateSong;
+    private String likeSong;
+    private String listenSong;
+    private String downloadSong;
+    private String commendSong;
+
+    public Song() {
+    }
+
+    public Song(int idSong, String nameSong, String infoSong, String imageSong, String dateSong, String likeSong, String listenSong, String downloadSong, String commendSong) {
+        this.idSong = idSong;
+        this.nameSong = nameSong;
+        this.infoSong = infoSong;
+        this.imageSong = imageSong;
+        this.dateSong = dateSong;
+        this.likeSong = likeSong;
+        this.listenSong = listenSong;
+        this.downloadSong = downloadSong;
+        this.commendSong = commendSong;
+    }
+
+    public int getIdSong() {
+        return idSong;
+    }
+
+    public void setIdSong(int idSong) {
+        this.idSong = idSong;
+    }
+
+    public String getNameSong() {
+        return nameSong;
+    }
+
+    public void setNameSong(String nameSong) {
+        this.nameSong = nameSong;
+    }
+
+    public String getInfoSong() {
+        return infoSong;
+    }
+
+    public void setInfoSong(String infoSong) {
+        this.infoSong = infoSong;
+    }
+
+    public String getImageSong() {
+        return imageSong;
+    }
+
+    public void setImageSong(String imageSong) {
+        this.imageSong = imageSong;
+    }
+
+    public String getDateSong() {
+        return dateSong;
+    }
+
+    public void setDateSong(String dateSong) {
+        this.dateSong = dateSong;
+    }
+
+    public String getLikeSong() {
+        return likeSong;
+    }
+
+    public void setLikeSong(String likeSong) {
+        this.likeSong = likeSong;
+    }
+
+    public String getListenSong() {
+        return listenSong;
+    }
+
+    public void setListenSong(String listenSong) {
+        this.listenSong = listenSong;
+    }
+
+    public String getDownloadSong() {
+        return downloadSong;
+    }
+
+    public void setDownloadSong(String downloadSong) {
+        this.downloadSong = downloadSong;
+    }
+
+    public String getCommendSong() {
+        return commendSong;
+    }
+
+    public void setCommendSong(String commendSong) {
+        this.commendSong = commendSong;
+    }
+}
